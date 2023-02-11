8c731e891abbfc902aa2cb18960ec18a2aadf8dc
Wahab Shah <wahab.shah@wiesheu.de>
2023-02-11 18:59:28 +0100

    added the compiled qt wasm application

--- build-Qt6QuickApp-Desktop-Debug/.qt_plugins/Qt6_QmlPlugins_Imports_Qt6QuickApp.cmake
+++
@@ -1,5 +0,0 @@
-set(qml_import_scanner_imports_count 3)
-set(qml_import_scanner_import_0 "CLASSNAME;QtQuick2Plugin;LINKTARGET;Qt6::qtquick2plugin;NAME;QtQuick;PATH;/usr/lib/x86_64-linux-gnu/qt6/qml/QtQuick;PLUGIN;qtquick2plugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQuick/;RELATIVEPATH;QtQuick;TYPE;module;")
-set(qml_import_scanner_import_1 "NAME;QtQml;PATH;/usr/lib/x86_64-linux-gnu/qt6/qml/QtQml;RELATIVEPATH;QtQml;TYPE;module;")
-set(qml_import_scanner_import_2 "CLASSNAME;QtQuick_WindowPlugin;LINKTARGET;Qt6::quickwindow;NAME;QtQuick.Window;PATH;/usr/lib/x86_64-linux-gnu/qt6/qml/QtQuick/Window;PLUGIN;quickwindowplugin;PREFER;:/qt-project.org/imports/QtQuick/Window/;RELATIVEPATH;QtQuick/Window;TYPE;module;")
-
--- build-Qt6QuickApp-Desktop-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/usr/bin/qmake6" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/usr" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
